4e22a0c4bc0ad5ff82c6c0a8a3c99f6d8bfc9b15
czuffetti <86202839+czuffetti@users.noreply.github.com>
2021-06-20 11:17:27 +0200

    some unit tests on teams and players

--- nba-teamsTests/nba_teamsTests.swift
+++ nba-teamsTests/nba_teamsTests.swift
@@ -23,6 +23,33 @@ class nba_teamsTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testTeam() {
+        let viewModel = Team(id: 1, abbreviation: "bs", city: "milano", conference: .east, division: .atlantic, fullName: "test team milano", name: "test team")
+        XCTAssert(viewModel.id == 1)
+    }
+    
+    func testEmptyTeams() {
+        let data: [Datum] = []
+        let meta: Meta = Meta(totalPages: 0, currentPage: 0, nextPage: 0, perPage: 0, totalCount: 0)
+        let viewModel = Teams.init(data: data, meta: meta)
+        XCTAssert(viewModel.data.count == 0)
+    }
+    
+    func testPlayer() {
+        let team = Team(id: 1, abbreviation: "bs", city: "milano", conference: .east, division: .atlantic, fullName: "test team milano", name: "test team")
+        let viewModel = DatumPlayer.init(id: 1, firstName: "Carlo", heightFeet: nil, heightInches: nil, lastName: "Zuffetti", position: .empty, team: team, weightPounds: nil)
+        
+        XCTAssert(viewModel.id == 1)
+        XCTAssert(viewModel.firstName == "Carlo")
+    }
+    
+    func testEmptyPlayers() {
+        let data: [DatumPlayer] = []
+        let meta: Meta = Meta(totalPages: 0, currentPage: 0, nextPage: 0, perPage: 0, totalCount: 0)
+        let viewModel = Players.init(data: data, meta: meta)
+        XCTAssert(viewModel.data.count == 0)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
